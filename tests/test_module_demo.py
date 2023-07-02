3b59358ea427affa2a8ae365e6f10b06bb836e3f
Jonathan Hannay <31388553+swfc@users.noreply.github.com>
2023-07-02 18:25:17 -0400

    Add tests.

---
+++ tests/test_module_demo.py
@@ -0,0 +1,9 @@
+import pytest
+from example_py_package.module_demo import print_something
+
+
+def test_print_something(capsys):
+    print_something("hello")
+    captured = capsys.readouterr()
+    assert captured.out == "hello\n"
+    assert captured.err == ""
