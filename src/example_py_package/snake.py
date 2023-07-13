ba90f94941df78b85f75591e3d1ebd6834ab7d2a
Jonathan Hannay <31388553+swfc@users.noreply.github.com>
2023-07-12 23:20:18 -0400

    Add some funcs to snake module.

--- src/example_py_package/snake.py
+++ src/example_py_package/snake.py
@@ -1,3 +1,22 @@
-def print_something(something: str) -> str:
-    print(something, flush=True)
-    return something
+SNAKE = r"""  \
+   \    __
+    \  {oo}
+       (__)\
+         λ \\
+           _\\__
+          (_____)_
+         (_______)Oo°
+"""
+
+
+def bubble(message):
+    bubble_length = len(message) + 2
+    return f"""
+ {"_" * bubble_length}
+( {message} )
+ {"-" * bubble_length}"""
+
+
+def say(message):
+    print(bubble(message))
+    print(SNAKE)
