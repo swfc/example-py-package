309844eb96513a2eb699c4541986fd5c527cded3
Jonathan Hannay <31388553+swfc@users.noreply.github.com>
2023-08-29 23:08:21 -0400

    Update version in __init__.

--- src/example_py_package/__init__.py
+++ src/example_py_package/__init__.py
@@ -1,7 +1,7 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version("example-py-package")
+    __version__ = version(__name__)
 except PackageNotFoundError:
     # package is not installed
     pass
